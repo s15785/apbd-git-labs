814866dec75134cd97ab8995b598a45fe2b0a2be
s15785 <s15785@pjwstk.edu.pl>
2024-03-24 17:02:44 +0100

    Zmiana zmiennej po stronie feature-new

--- apbd-git-labs/MathUtils.cs
+++ apbd-git-labs/MathUtils.cs
@@ -4,8 +4,8 @@ public class MathUtils
 {
     public static double Avg(int [] numbers)
     {
-        double sum = numbers.Sum();
-        return sum / numbers.Length;
+        double numbersSum = numbers.Sum();
+        return numbersSum / numbers.Length;
     }
     
     public static int Max(int [] numbers)
